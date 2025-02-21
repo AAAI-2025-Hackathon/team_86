8c9195303ef7540776c23bba032c88c3382a7605
Ashish Dhakal <aashishdhakak@gmail.com>
2025-02-21 16:13:52 +0300

    chore: make the function empty as its not used

--- src/client/content.js
+++ src/client/content.js
@@ -1,7 +1,3 @@
-// This script runs in the context of the web page
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === 'extractBody') {
-      const bodyContent = document.body.innerText;
-      sendResponse({ content: bodyContent });
-    }
-  });
+  // No actions will be taken, so the function is empty.
+});
